641766b92d74eb2829e3872da4f67c56b8857e44
YuriyGubin <iosdevelop.2022@gmail.com>
2022-10-18 23:36:40 +0500

    Working version

--- ContactList/ExtendedListViewController.swift
+++ ContactList/ExtendedListViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ExtendedListViewController: UITableViewController {
 
-    var persons: [Person]!
+    var persons: [Person] = []
 
     // MARK: - Table view data source
 
--- ContactList/TabBarViewController.swift
+++ ContactList/TabBarViewController.swift
@@ -8,24 +8,30 @@
 import UIKit
 
 class TabBarViewController: UITabBarController {
+    
+    private let persons = Person.getPersons()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTabBar()
+        prepareControllers()
+    }
+    
+    private func setupTabBar() {
         let tabBarAppearance = UITabBarAppearance()
         tabBarAppearance.configureWithOpaqueBackground()
         tabBar.standardAppearance = tabBarAppearance
         tabBar.scrollEdgeAppearance = tabBarAppearance
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func prepareControllers() {
+        guard let controllers = viewControllers else { return }
+        controllers.forEach { controller in
+            if let extendVC = controller as? ExtendedListViewController {
+                extendVC.persons = persons
+            }
+        }
     }
-    */
+    
 
 }
