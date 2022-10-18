0d5fd93b3dd17b46dcff339d34a61d510b0901a3
GubinYuriy <iosdevelop.2022@gmail.com>
2022-10-18 11:56:41 +0500

    Work with model

--- ContactList/Person.swift
+++ ContactList/Person.swift
@@ -9,16 +9,34 @@ struct Person {
     let name: String
     let surname: String
     let email: String
-    let phone: String
+    let phoneNumber: String
     
     var fullname: String {
         "\(name) \(surname)"
     }
     
+    static func getPerson() -> [Person] {
+        let data = DataManager()
+
+        let persons = [Person(
+            name: data.names[Int.random(in: 0..<data.names.count)],
+            surname: data.surnames[Int.random(in: 0..<data.surnames.count)],
+            email: data.emails[Int.random(in: 0..<data.emails.count)],
+            phoneNumber: data.phoneNumbers[Int.random(in: 0..<data.phoneNumbers.count)]
+        )
+        ]
+        return persons
+    }
+    
+    
+}
+
+class DataManager {
+    
     let names = [
         "Irina",
         "Ekaterina",
-        "Monica",
+        "Vera",
         "Elena",
         "Nataliya",
         "Polina",
@@ -29,6 +47,41 @@ struct Person {
     ]
     
     let surnames = [
+    "Petrova",
+    "Kirsanova",
+    "Tenyunina",
+    "Ivanova",
+    "Sidorova",
+    "Smetanina",
+    "Ovsova",
+    "Fedorova",
+    "Kalinina",
+    "Vaseneva"
+    ]
     
+    let emails = [
+        "bigeyes@gmail.com",
+        "nicesmile@gmail.com",
+        "kindsoul@gmail.com",
+        "elasticnut@gmail.com",
+        "cutyface@gmail.com",
+        "swanneck@gmail.com",
+        "rougecheeks@gmail.com",
+        "velvetskin@gmail.com",
+        "longlegs@gmail.com",
+        "clever@gmail.com"
+    ]
+    
+    let phoneNumbers = [
+    "+79256984128",
+    "+79586243569",
+    "+78965412354",
+    "+18569874125",
+    "+14587896521",
+    "+78546985698",
+    "+78456215489",
+    "+78362145487",
+    "+78523652497",
+    "+73658741020"
     ]
 }
